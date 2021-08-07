3d4b1e30d62e5a5f2daf8c400001665d9ac2ede5
Erik Sánchez Pérez <erik.sape@gmail.com>
2021-08-07 18:55:45 -0500

    feat: adding the class

---
+++ src/LogAnalyzer.php
@@ -0,0 +1,12 @@
+<?php
+class LogAnalyzer
+{
+    public function IsValidLogFileName(string $fileName): bool
+    {
+        if(str_ends_with($fileName, ".SLF"))
+        {
+            return false;
+        }
+        return true;
+    }
+}
