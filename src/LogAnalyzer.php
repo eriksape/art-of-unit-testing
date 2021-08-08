d4bc2b3636e401e7fb457069db800f9341542ed9
Erik Sánchez Pérez <erik.sape@gmail.com>
2021-08-08 08:39:13 -0500

    feat: added setup, teardown setting and exception test

--- src/LogAnalyzer.php
+++ src/LogAnalyzer.php
@@ -1,8 +1,16 @@
 <?php
 class LogAnalyzer
 {
+    public bool $wasLastFileValid;
+
     public function IsValidLogFileName(string $fileName): bool
     {
-        return str_ends_with(strtoupper($fileName), ".SLF");
+        $this->wasLastFileValid = false;
+        if (empty($fileName)) {
+            throw new Exception('filename has to be provided.');
+        }
+        $result = str_ends_with(strtoupper($fileName), ".SLF");
+        $this->wasLastFileValid = $result;
+        return $result;
     }
 }
--- tests/LogAnalyzerTest.php
+++ tests/LogAnalyzerTest.php
@@ -3,13 +3,20 @@ use PHPUnit\Framework\TestCase;
 
 class LogAnalyzerTest extends TestCase
 {
+    private ?LogAnalyzer $analyzer = null;
+
+    protected function setUp(): void
+    {
+        $this->analyzer = new LogAnalyzer();
+    }
+
     /**
+     * @group goodTest
      * @dataProvider filesToValidate
      */
     public function test_IsValidLogFileName_VariousExtensions_ChecksThem(string $filename, bool $expected): void
     {
-        $analyzer = new LogAnalyzer();
-        $result = $analyzer->IsValidLogFileName($filename);
+        $result = $this->analyzer->IsValidLogFileName($filename);
         $this->assertEquals($expected, $result);
     }
 
@@ -20,5 +27,39 @@ class LogAnalyzerTest extends TestCase
             ['filewithgoodextension.slf', true],
             ['filewithgoodextension.SLF', true],
         ];
+    }
+
+    /**
+     * @group goodTest
+     */
+    public function test_IsValidFileName_EmptyFileName_ThrowsException():void
+    {
+        $this->expectExceptionMessage("filename has to be provided");
+        $this->analyzer->IsValidLogFileName("");
+    }
+
+    /**
+     * @group testSkipped
+     */
+    public function test_IsAboutNewThing():void
+    {
+        $this->markTestSkipped("not implemented yet.");
+    }
+
+    /**
+     * @group goodTest
+     * @dataProvider filesToValidate
+     */
+    public function test_IsValidFileName_WhenCalled_ChangesWasLastFileNameValid(string $filename, bool $expected):void
+    {
+        $this->analyzer->IsValidLogFileName($filename);
+        $this->assertEquals($expected, $this->analyzer->wasLastFileValid);
+    }
+
+    protected function tearDown(): void
+    {
+        //the line below is included to show an anti pattern.
+        //This isn’t really needed. Don’t do it in real life.
+        $this->analyzer = null;
     }
 }
