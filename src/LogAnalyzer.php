ec52ebc0021bf1551a617a65e72346e9841386f4
Erik Sánchez Pérez <erik.sape@gmail.com>
2021-08-07 19:00:43 -0500

    refactor: disabled the extension case sensitive

--- src/LogAnalyzer.php
+++ src/LogAnalyzer.php
@@ -3,7 +3,7 @@ class LogAnalyzer
 {
     public function IsValidLogFileName(string $fileName): bool
     {
-        if(!str_ends_with($fileName, ".SLF"))
+        if(!str_ends_with(strtoupper($fileName), ".SLF"))
         {
             return false;
         }
